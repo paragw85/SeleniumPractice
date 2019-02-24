8fb2c47894ed93506bbf455ff03263c6dc74bb6c
Parag wani <44331651+paragwani1985@users.noreply.github.com>
2019-02-24 09:46:57 +0530

    24022019

--- src/SeleniumPract/FirstSelenium.java
+++ src/SeleniumPract/FirstSelenium.java
@@ -20,7 +20,7 @@ public class FirstSelenium {
 
 			System.out.println("Currect Website and Commit");
 		}else{
-			System.out.println("Wrong Website");
+			System.out.println("Wrong Website 00001");
 		}
 		System.out.println(driver.getPageSource().length());
 		driver.quit();
